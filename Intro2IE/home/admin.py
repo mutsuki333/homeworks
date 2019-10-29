f67d0c41647f60a15f51eb8a14d13721bfa94d6c
Evan <evanchen333@gmail.com>
2019-10-29 15:46:39 +0800

    add record admin

--- Intro2IE/home/admin.py
+++ Intro2IE/home/admin.py
@@ -6,7 +6,11 @@ admin.site.site_header = '庫存後台管理'
 admin.site.unregister(Group)
 
 
-from .models import Item
+from .models import Item, Record
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name','stock','description','picture')
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('item_name','alter','date')
