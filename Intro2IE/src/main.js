ba2621c4c08eb191767887f626b994af6291d4c7
Evan <evanchen333@gmail.com>
2019-12-17 15:14:13 +0800

    update

--- Intro2IE/src/main.js
+++ Intro2IE/src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = 'http://218.161.27.75/Intro2IE/api';
 axios.defaults.withCredentials = true;
 axios.defaults.debug = true;
 axios.defaults.xsrfCookieName = 'csrftoken'
